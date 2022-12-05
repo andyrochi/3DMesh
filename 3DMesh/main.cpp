279dbc0ccf6191346afaeb0256b33ab5d1aa8e3a
andyrochi <andy.rochi@gmail.com>
2022-12-05 14:07:17 +0800

    feat: trackball feature

--- 3DMesh/main.cpp
+++ 3DMesh/main.cpp
@@ -9,6 +9,18 @@ std::vector<floatvec> vertices;
 std::vector<floatvec> normals;
 std::vector<std::vector<GLushort>> DinoOrder;
 
+const int refreshMillis = 30;
+
+struct vector2d {
+	double x, y;
+};
+
+bool dragging = false;
+const double ROTATEFACTOR = 400.0;
+
+vector2d mouseLastPos = { 0, 0 };
+
+
 // Handles the keyboard event: the left and right arrows bend the elbow, the
 // up and down keys bend the shoulder.
 void special(int key, int, int) {
@@ -28,21 +40,50 @@ void key(unsigned char key, int, int) {
 	glutPostRedisplay();
 }
 
+void mouseDown(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		mouseLastPos.x = x;
+		mouseLastPos.y = y;
+		dragging = true;
+		printf("drag! %d, %d\n", x, y);
+		glutSetCursor(GLUT_CURSOR_CROSSHAIR);
+	}
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+		dragging = false;
+		printf("release! %d, %d\n", x, y);
+		glutSetCursor(GLUT_CURSOR_LEFT_ARROW);
+	}
+}
+
+void mouseMove(int x, int y) {
+	if (dragging == true) {
+		printf("move! %d, %d\n", x, y);
+		double dPhi = (mouseLastPos.y - y) / ROTATEFACTOR;
+		double dTheta = (mouseLastPos.x - x) / ROTATEFACTOR;
+			mouseLastPos.x = x;
+			mouseLastPos.y = y;
+		printf("dPhi: %.3f, dTheta: %.3f\n", dPhi, dTheta);
+		camera.rotate(dTheta, dPhi);
+	}
+}
+
 
 void display() {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	glMatrixMode(GL_MODELVIEW);
 
 	glLoadIdentity();
 	floatvec up = camera.getCameraUp();
+
 	gluLookAt(camera.getX(), camera.getY(), camera.getZ(),
 		centerX, centerY, centerZ,
 		up[0], up[1], up[2]);
 
 	draw_obj(vertices, normals, DinoOrder);
 
-	//drawAxis();
+	drawAxis();
 
 	glutSwapBuffers();
 }
@@ -56,6 +97,13 @@ void reshape(GLint w, GLint h) {
 	glLoadIdentity();
 	gluPerspective(60.0, GLfloat(w) / GLfloat(h), 0.01, 5.0);
 	glMatrixMode(GL_MODELVIEW);
+}
+
+/* Called back when the timer expired, used to rerender display */
+void refreshDisplay(int value) {
+	glutPostRedisplay();    // Post a paint request to activate display()
+	glutTimerFunc(refreshMillis, refreshDisplay, 0); // subsequent timer call at milliseconds
+
 }
 
 void init() {
@@ -96,6 +144,9 @@ int main(int argc, char** argv) {
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(key);
 	glutSpecialFunc(special);
+	glutMotionFunc(mouseMove);
+	glutMouseFunc(mouseDown);
+	glutTimerFunc(0, refreshDisplay, 0);   // Refresh rate
 	init();
 	glutMainLoop();
 }
