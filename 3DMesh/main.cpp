d88ec71ca818d80329bed4299c730752ba6262bc
andyrochi <andy.rochi@gmail.com>
2022-12-05 16:41:34 +0800

    feat: Add speed during rotation

--- 3DMesh/main.cpp
+++ 3DMesh/main.cpp
@@ -10,16 +10,36 @@ std::vector<floatvec> normals;
 std::vector<std::vector<GLushort>> DinoOrder;
 
 const int refreshMillis = 30;
+const int speedTickMillis = 100;
 
 struct vector2d {
 	double x, y;
 };
 
 bool dragging = false;
-const double ROTATEFACTOR = 400.0;
+const double ROTATEFACTOR = 500.0;
 
-vector2d mouseLastPos = { 0, 0 };
+vector2d mouseSpeed = { 0, 0 };
+vector2d mouseCurPos = { 0, 0 };
+vector2d mousePrevPos = { 0, 0 };
+vector2d cameraSpeed = { 0, 0 };
 
+// call every speedTickMillis
+const int momentum = 0.1;
+void updateMousePos() {
+	if (dragging) {
+		mouseSpeed.x = mouseCurPos.x - mousePrevPos.x;
+		mouseSpeed.y = mouseCurPos.y - mousePrevPos.y;
+		mousePrevPos.x = mouseCurPos.x;
+		mousePrevPos.y = mouseCurPos.y;
+		cameraSpeed.x = mouseSpeed.x / ROTATEFACTOR;
+		cameraSpeed.y = mouseSpeed.y / ROTATEFACTOR;
+	}
+	else {
+		cameraSpeed.x *= 0.8;
+		cameraSpeed.y *= 0.8;
+	}
+}
 
 // Handles the keyboard event: the left and right arrows bend the elbow, the
 // up and down keys bend the shoulder.
@@ -40,11 +60,18 @@ void key(unsigned char key, int, int) {
 	glutPostRedisplay();
 }
 
-void mouseDown(int button, int state, int x, int y) {
+void mouseFunc(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		mouseLastPos.x = x;
-		mouseLastPos.y = y;
+		if (dragging == false) {
+			mousePrevPos.x = x;
+			mousePrevPos.y = y;
+			mouseCurPos.x = x;
+			mouseCurPos.y = y;
+			cameraSpeed.x = 0;
+			cameraSpeed.y = 0;
+		}
 		dragging = true;
+		
 		printf("drag! %d, %d\n", x, y);
 		glutSetCursor(GLUT_CURSOR_CROSSHAIR);
 	}
@@ -57,13 +84,8 @@ void mouseDown(int button, int state, int x, int y) {
 
 void mouseMove(int x, int y) {
 	if (dragging == true) {
-		printf("move! %d, %d\n", x, y);
-		double dPhi = (mouseLastPos.y - y) / ROTATEFACTOR;
-		double dTheta = (mouseLastPos.x - x) / ROTATEFACTOR;
-			mouseLastPos.x = x;
-			mouseLastPos.y = y;
-		printf("dPhi: %.3f, dTheta: %.3f\n", dPhi, dTheta);
-		camera.rotate(dTheta, dPhi);
+		mouseCurPos.x = x;
+		mouseCurPos.y = y;
 	}
 }
 
@@ -101,9 +123,14 @@ void reshape(GLint w, GLint h) {
 
 /* Called back when the timer expired, used to rerender display */
 void refreshDisplay(int value) {
+	camera.rotate(-cameraSpeed.x, -cameraSpeed.y);
 	glutPostRedisplay();    // Post a paint request to activate display()
 	glutTimerFunc(refreshMillis, refreshDisplay, 0); // subsequent timer call at milliseconds
+}
 
+void speedTick(int value) {
+	updateMousePos();
+	glutTimerFunc(speedTickMillis, speedTick, 0); // subsequent timer call at milliseconds
 }
 
 void init() {
@@ -145,8 +172,9 @@ int main(int argc, char** argv) {
 	glutKeyboardFunc(key);
 	glutSpecialFunc(special);
 	glutMotionFunc(mouseMove);
-	glutMouseFunc(mouseDown);
+	glutMouseFunc(mouseFunc);
 	glutTimerFunc(0, refreshDisplay, 0);   // Refresh rate
+	glutTimerFunc(0, speedTick, 0);
 	init();
 	glutMainLoop();
 }
