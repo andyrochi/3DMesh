2a600395cee68a284fcd0ac832f063dfbcbc1845
andyrochi <andy.rochi@gmail.com>
2022-12-06 00:23:40 +0800

    style: modify getCameraUp

--- 3DMesh/Camera.h
+++ 3DMesh/Camera.h
@@ -12,31 +12,32 @@ class Camera {
 	double phi;
 	double dPhi;
 	double radius;
+	floatvec up;
 public:
-	Camera() : theta(0), dTheta(0.04), phi(M_PI/2), dPhi(0.04), radius(2.0) {}
+	Camera() : theta(0), dTheta(0.04), phi(M_PI / 2), dPhi(0.04), radius(2.0), up({0.0,0.0, 1.0}) {}
 	double getX() { return radius * sin(phi) * cos(theta); }
 	double getY() { return radius * sin(phi) * sin(theta); }
 	double getZ() { return radius * cos(phi); }
 	void moveRight() {
-		theta += dTheta;
+		theta += this->dTheta;
 		if (theta >= TWO_PI) 
 			theta = 0;
 	}
-	void moveLeft() { theta -= dTheta; if (theta <= 0) theta = TWO_PI - dTheta; }
-	void moveUp() { phi -= dPhi; if (phi <= 0.001) phi = 0.001; }
-	void moveDown() { phi += dPhi; if (phi >= M_PI) phi = M_PI; }
+	void moveLeft() { theta -= this->dTheta; if (theta <= 0) theta = TWO_PI - this->dTheta; }
+	void moveUp() { phi -= this->dPhi; if (phi <= 0.001) phi = 0.001; }
+	void moveDown() { phi += this->dPhi; if (phi >= M_PI) phi = M_PI; }
 
-	void rotate(double dTheta, double dPhi) {
-		theta += dTheta;
-		phi += dPhi;
+	void rotate(double dThetaP, double dPhiP, bool drag = false) {
+		phi += dPhiP;
+		theta += dThetaP;
 	}
 
 	floatvec getCameraUp() {
 		floatvec posVec = { float(getX()), float(getY()), float(getZ()) };
 		normalize(posVec);
-		floatvec worldUp = { 0.0f, 0.0f, 1.0f };
+		/*floatvec worldUp = { 0.0f, 0.0f, 1.0f };*/
 		
-		floatvec left = crossProduct(posVec, worldUp);
+		floatvec left = crossProduct(posVec, up);
 		normalize(left);
 		floatvec up = crossProduct(left, posVec);
 		normalize(up);
